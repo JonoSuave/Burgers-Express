caf20d39ee73501fd1702f9fea65957f534ed992
JonoSuave <JonoSuave@gmail.com>
2019-08-28 00:43:29 -0600

    Trying to consume api-server from react-client
    Currently the axios.get(url) request is failing

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -2,9 +2,28 @@ import React, { Fragment } from 'react'
 import Burger from './Burgers';
 import NavbarPage from './Navbar';
 import {Container} from 'react-bootstrap';
+import axios from 'axios';
 
 
 class App extends React.Component {
+  state = {
+    burgers: [],
+    fries: [],
+    shakes: [],
+    users: []
+  };
+  componentDidMount() {
+    axios.get("http://localhost:4000/api/burgers")
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+            this.setState({ burgers: data.burgers })
+        })
+        .catch(error => {
+          console.log(error);
+        })
+  }
+
   render() {
     return (
       <Fragment>
---
+++ react-client/src/api.js
@@ -0,0 +1,3 @@
+export default {
+    posts: 'http://localhost:4000/posts'
+}
