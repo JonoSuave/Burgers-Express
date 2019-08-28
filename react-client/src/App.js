aa86ca02c47548c595553e402e2c1f66f664f4f2
JonoSuave <JonoSuave@gmail.com>
2019-08-28 16:50:27 -0600

    Set up client home page

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import Burger from './Burgers';
 import NavbarPage from './Navbar';
 import {Container} from 'react-bootstrap';
 import axios from 'axios';
@@ -27,9 +26,31 @@ class App extends React.Component {
   render() {
     return (
       <Fragment>
-        <NavbarPage />
         <Container>
-          <Burger />
+        <NavbarPage />
+
+          <div id="home-page">
+            <h1 id="title-message">Welcome to Burgers Express</h1>
+            <div id="subNav">
+              <ul id="home-list-style">
+                <li className="favicon">
+                  <img src="./images/favicons/nav_icon_burger.png" alt="" />
+                  <a className="menu-text" href="/burgers">Burgers</a>
+                </li>
+
+                <li className="favicon">
+                  <img src="./images/favicons/nav_icon_fries.png" alt="" />
+                  <a className="menu-text" href="/fries">Fries</a>
+                </li>
+
+                <li className="favicon">
+                  <img src="./images/favicons/nav_icon_cup.png" alt="" />
+                  <a className="menu-text" href="/shakes">Shakes</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
         </Container>
       </Fragment>
 
